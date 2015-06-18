0aa3e314c7309a46cee7e638bea948b9f5dad020
Le Tuan Anh <tuananh.ke@gmail.com>
2015-06-19 03:30:04 +0800

    Add Table, TextReport and fix logging method

---
+++ leutile.demo.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+leutile demo code
+Latest version can be found at https://github.com/letuananh/lelesk
+
+@author: Le Tuan Anh <tuananh.ke@gmail.com>
+'''
+
+# Copyright (c) 2015, Le Tuan Anh <tuananh.ke@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+__author__ = "Le Tuan Anh <tuananh.ke@gmail.com>"
+__copyright__ = "Copyright 2015, chirptext"
+__credits__ = [ "Le Tuan Anh" ]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Le Tuan Anh"
+__email__ = "<tuananh.ke@gmail.com>"
+__status__ = "Prototype"
+
+#-----------------------------------------------------------------------
+
+from leutile import jilog, header, Counter, Timer, TextReport, Table, LOREM_IPSUM
+from itertools import cycle
+#-----------------------------------------------------------------------
+
+def generate_report(report):
+    report.header("A beautiful dummy report", level='h0')
+    report.print("An introduction to the art of creating text report.")
+    report.print(LOREM_IPSUM)
+
+    # demo sections & subsections
+    for section in range(1,3):
+        report.header("Section %s" % section, level='h1')
+        report.print(LOREM_IPSUM[:70])
+        for subsect in range(1,3):
+            report.header("Subsection %s" % subsect, level='h2')
+            for k in range(1,4):
+                report.print("Line %s" % k, level=1)
+
+    # Demo draw table
+    report.header('Summary Table')
+    report.print("Here is a demo table", level=1)
+    # Create a table
+    tbl = Table()
+    words = cycle(LOREM_IPSUM.split())
+    tbl.add_row(["Index", "Word #1", "Word #2", "Word #3", "Word #4"])
+    for i in range(3):
+        row = [ i ] + [ next(words) for j in range(4) ]
+        tbl.add_row(row)
+    # print the table to report file
+    print_tbl = lambda x: report.print(x, level=1)
+    tbl.print(print_func=print_tbl)
+
+    # Done!
+    report.close()
+
+def main():
+    header("Main method")
+    c = Counter()
+    t = Timer()
+    
+    t.start("Doing some time-consuming tasks ...")
+
+    jilog("Count even & odd numbers ...")
+    for i in range(10000):
+        if i % 2 == 0:
+            c.count("even")
+        else:
+            c.count("odd")
+    c.summarise()
+
+    
+    report = TextReport("~/tmp/foo.txt")
+    jilog("Now try to create a text report (Located at: %s)" % (report.get_path()))
+    generate_report(report)
+
+    # try to report to stdout
+    jilog("The same report to stdout ...")
+    generate_report(TextReport())
+    
+    t.end("Done")
+
+if __name__ == "__main__":
+    main()
--- leutile.py
+++ leutile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #Copyright (c) 2014, Le Tuan Anh <tuananh.ke@gmail.com>
@@ -28,9 +28,9 @@ import time
 import itertools
 import operator
 if sys.version_info >= (3, 0):
-	from itertools import zip_longest
+    from itertools import zip_longest
 else:
-	from itertools import izip_longest
+    from itertools import izip_longest
 
 from collections import OrderedDict
 # from chirptext.leutile import jilog, Timer, Counter, StringTool
@@ -40,184 +40,316 @@ class ChirpConfig:
         JILOG_LOCATION = None # ChirpConfig.JILOG_LOCATION = 'debug.txt'
 
 def jilog(msg):
-	try:
-                if 'e' in ChirpConfig.JILOG_OUTPUT:
-                        sys.stderr.write(("%s\n" % str(msg)).
-                                         encode("ascii","ignore").decode('ascii', 'ignore'))
-                if 'o' in ChirpConfig.JILOG_OUTPUT:
-                        sys.stdout.write(("%s\n" % str(msg)).
-                                         encode("ascii","ignore").decode('ascii', 'ignore'))
-	except:
-		# nah, dun care
-		pass
-	try:
-                if ChirpConfig.JILOG_LOCATION:
-                        with codecs.open(JILOG_LOCATION, "a", encoding='utf-8') as logfile:
-                                logfile.write("%s\n" % str(msg))
-                                pass
-	except Exception as ex:
-		# sys.stderr.write(str(ex))
-		# nah, dun care
-		pass
+    try:
+        if 'e' in ChirpConfig.JILOG_OUTPUT:
+            sys.stderr.write(("%s\n" % str(msg)).
+                encode("ascii","ignore").decode('ascii', 'ignore'))
+        if 'o' in ChirpConfig.JILOG_OUTPUT:
+            sys.stdout.write(("%s\n" % str(msg)).
+                encode("ascii","ignore").decode('ascii', 'ignore'))
+    except:
+        # nah, dun care
+        pass
+    try:
+        if ChirpConfig.JILOG_LOCATION:
+            with codecs.open(JILOG_LOCATION, "a", encoding='utf-8') as logfile:
+                logfile.write("%s\n" % str(msg))
+                pass
+    except Exception as ex:
+        # sys.stderr.write(str(ex))
+        # nah, dun care
+        pass
 
 def uniquify(a_list):
-	return list(OrderedDict(zip(a_list, range(len(a_list)))).keys())
+    return list(OrderedDict(zip(a_list, range(len(a_list)))).keys())
 
-def header(msg, level='h1'):
-    if level == 'h1':
-        print('-' * 80)
-        print(msg)
-        print('-' * 80)
+def header(msg, level='h1', print_out=print):
+    if level == 'h0':
+        # box_len = 80 if len(msg) < 80 else len(msg)
+        box_len = 80
+        print_out('+' + '-' * (box_len + 2))
+        print_out("| %s" % msg)
+        print_out('+' + '-' * (box_len + 2))
+    elif level == 'h1':
+        print_out("")
+        print_out('%s' % msg)
+        print_out('' + ('-' * 60))
     elif level == 'h2':
-        print('\t%s' % msg)
-        print('\t' + ('-' * 40))
+        print_out('\t%s' % msg)
+        print_out('\t' + ('-' * 40))
     else:
-        print('\t\t%s' % msg)
-        print('\t\t' + ('-' * 20))
+        print_out('\t\t%s' % msg)
+        print_out('\t\t' + ('-' * 20))
 
+class TextReport:
+    def __init__(self, report_path=None, mode='w', auto_flush=True):
+        ''' Create a text report.
+
+        Arguments:
+            report_path -- Path to report file
+            mode        -- a for append, w (default) for create from scratch (overwrite existing file)
+        '''
+        if not report_path:
+            self.report_path = "* stdout *"
+            self.report_file = sys.stdout
+            self.mode        = None
+            self.auto_flush  = False
+            pass
+        else:
+            self.report_path = os.path.expanduser(report_path)
+            self.report_file = open(self.report_path, mode)
+            self.auto_flush  = auto_flush
+            self.mode        = mode
+        self.print       = self.writeline # just an alias
+
+    def write(self, msg, level=0):
+        self.report_file.write("\t" * level)
+        self.report_file.write(msg)
+        if self.auto_flush:
+            self.report_file.flush()
+
+    def writeline(self, msg, level=0):
+        self.write("%s\n" % msg, level)
+
+    def header(self, msg, level='h1'):
+        header(msg, level, print_out=self.writeline)
+
+    def close(self):
+        if self.mode:
+            try:
+                self.report_file.flush()
+                self.report_file.close()
+            except Exception as e:
+                print("Error raised while saving report: %s" % e)
+
+    def get_path(self):
+        return self.report_path
 
 class Timer:
-	''' Timer a task
-	'''
-	def __init__(self):
-		self.start_time = time.time()
-		self.end_time = time.time()
-		
-	def start(self, task_note=''):
-		if task_note:
-			jilog("[%s]\n" % (str(task_note),))
-		self.start_time = time.time()
-		return self
-			
-	def stop(self):
-		self.end_time = time.time()
-		return self
-		
-	def __str__(self):
-		return "Execution time: %.2f sec(s)" % (self.end_time - self.start_time)
-		
-	def log(self, task_note = ''):
-		jilog("%s - Note=[%s]\n" % (self, str(task_note)))
-		return self
-		
-	def end(self, task_note=''):
-			self.stop().log(task_note)
+    ''' Timer a task
+    '''
+    def __init__(self):
+        self.start_time = time.time()
+        self.end_time = time.time()
+        
+    def start(self, task_note=''):
+        if task_note:
+            jilog("[%s]" % (str(task_note),))
+        self.start_time = time.time()
+        return self
+            
+    def stop(self):
+        self.end_time = time.time()
+        return self
+        
+    def __str__(self):
+        return "Execution time: %.2f sec(s)" % (self.end_time - self.start_time)
+        
+    def log(self, task_note = ''):
+        jilog("%s - Note=[%s]" % (self, str(task_note)))
+        return self
+        
+    def end(self, task_note=''):
+            self.stop().log(task_note)
 
 class Counter:
-	def __init__(self, priority=None):
-		self.count_map = {}
-		self.priority = priority if priority else []
-	
-	def __getitem__(self, key):
-		if key not in self.count_map:
-			self.count_map[key] = 0
-		return self.count_map[key]
-	
-	def __setitem__(self, key, value):
-		self.count_map[key] = value
-	
-	def count(self, key):
-		self[key] += 1
+    def __init__(self, priority=None):
+        self.count_map = {}
+        self.priority = priority if priority else []
+    
+    def __getitem__(self, key):
+        if key not in self.count_map:
+            self.count_map[key] = 0
+        return self.count_map[key]
+    
+    def __setitem__(self, key, value):
+        self.count_map[key] = value
 
-	def order(self, priority):
-		self.priority = [ x for x in priority ]
+    def __len__(self):
+        return len(self.count_map)
+    
+    def count(self, key):
+        self[key] += 1
 
-	def get_report_order(self):
-		order_list = []
-		for x in self.priority:
-			order_list.append([x, self[x]])
-		for x in sorted(list(self.count_map.keys())):
-			if x not in self.priority:
-				order_list.append([x, self[x]])
-		return order_list
-		
-	def summarise(self):
-		for k, v in self.get_report_order():
-			print( "%s: %d" % (k, v) )
+    def order(self, priority):
+        self.priority = [ x for x in priority ]
 
-	def save(self, file_loc):
-		'''Save counter information to files'''
-		with open(file_loc, 'w') as outfile:
-			for k, v in self.get_report_order():
-				outfile.write( "%s: %d\n" % (k, v) )
+    def get_report_order(self):
+        order_list = []
+        for x in self.priority:
+            order_list.append([x, self[x]])
+        for x in sorted(list(self.count_map.keys())):
+            if x not in self.priority:
+                order_list.append([x, self[x]])
+        return order_list
+        
+    def summarise(self):
+        for k, v in self.get_report_order():
+            print( "%s: %d" % (k, v) )
 
-	def sorted_by_count(self):
-		if sys.version_info >= (3, 0):
-			return sorted(self.count_map.items(), key=operator.itemgetter(1), reverse=True)
-		else:
-			return sorted(self.count_map.iteritems(), key=operator.itemgetter(1), reverse=True)
+    def save(self, file_loc):
+        '''Save counter information to files'''
+        with open(file_loc, 'w') as outfile:
+            for k, v in self.get_report_order():
+                outfile.write( "%s: %d\n" % (k, v) )
+
+    def sorted_by_count(self):
+        if sys.version_info >= (3, 0):
+            return sorted(self.count_map.items(), key=operator.itemgetter(1), reverse=True)
+        else:
+            return sorted(self.count_map.iteritems(), key=operator.itemgetter(1), reverse=True)
 
 class StringTool:
-	@staticmethod
-	def strip(a_str):
-		return a_str.strip() if a_str else ''
-	
-	@staticmethod
-	def to_str(a_str):
-		return str(a_str) if a_str else ''
+    @staticmethod
+    def strip(a_str):
+        return a_str.strip() if a_str else ''
+    
+    @staticmethod
+    def to_str(a_str):
+        return str(a_str) if a_str else ''
 
 class FileHub:
-	def __init__(self, ext='.log'):
-		self.files = {}
-		self.ext = ext if ext else ''
-	
-	def __getitem__(self, key):
-		if not self.files.has_key(key):
-			self.addtext(key)
-		return self.files[key]
-	
-	def __setitem__(self, key, value):
-		self.files[key] = value
-		
-	def addtext(self, key):
-		self.files[key] = open(key + self.ext, 'a')
-		
-	def create(self, key):
-		self.files[key] = open(key + self.ext, 'w')
-		
-	def writeline(self, key, text):
-		self[key].write('%s\n' % (text,))
-		self[key].flush()
-	
-	def flush(self):
-		for key in self.files.keys():
-			self[key].flush()
-			
-	def close(self):
-		for key in self.files.keys():
-			self[key].flush()
-			self[key].close()
+    def __init__(self, ext='.log'):
+        self.files = {}
+        self.ext = ext if ext else ''
+    
+    def __getitem__(self, key):
+        if not self.files.has_key(key):
+            self.addtext(key)
+        return self.files[key]
+    
+    def __setitem__(self, key, value):
+        self.files[key] = value
+        
+    def addtext(self, key):
+        self.files[key] = open(key + self.ext, 'a')
+        
+    def create(self, key):
+        self.files[key] = open(key + self.ext, 'w')
+        
+    def writeline(self, key, text, auto_flush=True):
+        self[key].write('%s\n' % (text,))
+        if auto_flush:
+            self[key].flush()
+    
+    def flush(self):
+        for key in self.files.keys():
+            self[key].flush()
+            
+    def close(self):
+        for key in self.files.keys():
+            self[key].flush()
+            self[key].close()
+
+class Table:
+    def __init__(self, header=True, padding=True, NoneValue=None):
+        self.rows = []
+        self.col_count = 0
+        self.NoneValue = NoneValue
+        self.max_lengths = []
+        self.header = header
+        self.padding = padding
+
+    def maxlength(self, val_a, val_b):
+        len_a = len(val_a) if val_a else 0
+        len_b = len(val_b) if val_b else 0
+        return len_a if len_a > len_b else len_b
+
+    def add_row(self, new_row):
+        if new_row is None:
+            raise ValueError("Row cannot be None")
+        if len(new_row) > self.col_count:
+            self.col_count = len(new_row) # longer row
+            for row in self.rows:
+                # add cell
+                row += [ self.NoneValue ] * (len(new_row) - self.col_count)
+        elif len(new_row) < self.col_count:
+            new_row += [ self.NoneValue ] * (self.col_count - len(new_row))
+        self.rows.append(list(new_row)) # clone a list, rather than store the ref passed in
+
+    def __getitem__(self, row_id):
+        return list(self.rows[row_id])    # clone a row to return
+
+    def get_column(self, col_id):
+        return [ x[col_id] if x[col_id] is not None else self.NoneValue for x in self.rows ]
+
+    def format(self):
+        ''' Format table to print out
+        '''
+        self.max_lengths = []
+        for row in self.rows:
+            if len(self.max_lengths) < len(row):
+                self.max_lengths += [ 0 ] * (len(row) - len(self.max_lengths))
+            for idx, val in enumerate(row):
+                len_cell = len(str(val)) if val else 0
+                if self.max_lengths[idx] < len_cell:
+                    self.max_lengths[idx] = len_cell
+        return self.max_lengths
+
+    def print_separator(self, print_func):
+        self.print_cells([ '-' * (x + (2 if self.padding else 0)) for x in self.max_lengths ], print_func, joint='+')
+
+    def print_cells(self, cells, print_func=print, extra_lines = False, joint='|'):
+        print_func(joint + joint.join(cells) + joint)
+        if extra_lines:
+            self.print_separator(print_func)
+
+    def print(self, print_func=print):
+        max_lengths = self.format()
+        self.print_separator(print_func)
+        for ridx, row in enumerate(self.rows):
+            cells = []
+            for idx, cell in enumerate(row):
+                txt = str(cell)
+                cell_width = max_lengths[idx]
+                if ridx == 0 and self.header:
+                    txt = txt.center(cell_width)
+                else:
+                    txt = txt.rjust(cell_width) if is_number(txt) else txt.ljust(cell_width)
+                if self.padding:
+                    txt = ' ' + txt + ' '
+                cells.append(txt)
+            self.print_cells(cells, print_func, extra_lines=(self.header and ridx == 0))
+        self.print_separator(print_func)
+
+def is_number(s):
+    try:
+        if str(float(s)) != 'nan':
+            return True
+    except Exception:
+        pass
+    return False
 
 class FileTool:
-	@staticmethod
-	def getfilename(file_path):
-		''' Get filename without extension
-		'''
-		return os.path.splitext(getfullfilename(file_path))[0]
+    @staticmethod
+    def getfilename(file_path):
+        ''' Get filename without extension
+        '''
+        return os.path.splitext(getfullfilename(file_path))[0]
 
-	@staticmethod
-	def getfullfilename(file_path):
-		''' Get full filename (with extension)
-		'''
-		if file_path:
-			return os.path.basename(file_path)
-		else:
-			return ''
+    @staticmethod
+    def getfullfilename(file_path):
+        ''' Get full filename (with extension)
+        '''
+        if file_path:
+            return os.path.basename(file_path)
+        else:
+            return ''
+
+LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 def main():
-	print("This is an utility module, not an application.")
-	pass
+    print("This is an utility module, not an application.")
+    pass
 
 if __name__ == "__main__":
-	main()
+    main()
 
 if sys.version_info >= (3, 0):
-	def grouper(iterable, n, fillvalue=None):
-		args = [iter(iterable)] * n
-		return zip_longest(fillvalue=fillvalue, *args)
+    def grouper(iterable, n, fillvalue=None):
+        args = [iter(iterable)] * n
+        return zip_longest(fillvalue=fillvalue, *args)
 else:
-	def grouper(iterable, n, fillvalue=None):
-		args = [iter(iterable)] * n
-		return izip_longest(fillvalue=fillvalue, *args)
-
-
+    def grouper(iterable, n, fillvalue=None):
+        args = [iter(iterable)] * n
+        return izip_longest(fillvalue=fillvalue, *args)
