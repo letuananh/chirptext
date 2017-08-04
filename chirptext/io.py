b1618cb30660ddb2617234cfa017559823c97a4a
Le Tuan Anh <tuananh.ke@gmail.com>
2017-08-04 15:21:13 +0800

    Support CSV reading/writing

---
+++ chirptext/io.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+'''
+Readers for popular formats
+Latest version can be found at https://github.com/letuananh/chirptext
+
+References:
+    Python documentation:
+        https://docs.python.org/
+    PEP 0008 - Style Guide for Python Code
+        https://www.python.org/dev/peps/pep-0008/
+    PEP 257 - Python Docstring Conventions:
+        https://www.python.org/dev/peps/pep-0257/
+
+@author: Le Tuan Anh <tuananh.ke@gmail.com>
+'''
+
+# Copyright (c) 2017, Le Tuan Anh <tuananh.ke@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+__author__ = "Le Tuan Anh"
+__email__ = "<tuananh.ke@gmail.com>"
+__copyright__ = "Copyright 2017, chirptext"
+__license__ = "MIT"
+__maintainer__ = "Le Tuan Anh"
+__version__ = "0.1"
+__status__ = "Prototype"
+__credits__ = []
+
+########################################################################
+
+import os
+import csv
+import logging
+from collections import defaultdict as dd
+from collections import namedtuple
+
+from .leutile import FileHelper
+
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+#-------------------------------------------------------------------------------
+# DATA STRUCTURES
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# FUNCTIONS
+#-------------------------------------------------------------------------------
+
+class CSV(object):
+
+    @staticmethod
+    def read(file_name, dialect=None, header=False):
+        with open(file_name, newline='') as csvfile:
+            if not dialect:
+                # auto detect
+                dialect = csv.Sniffer().sniff(csvfile.read(1024))
+                csvfile.seek(0)
+            if not header:
+                reader = csv.reader(csvfile, dialect=dialect)
+                return list(reader)
+            else:
+                reader = csv.DictReader(csvfile, dialect=dialect)
+                return list(reader)
+
+    @staticmethod
+    def write(file_name, rows, dialect='excel', header=None, quoting=csv.QUOTE_ALL, extrasaction='ignore'):
+        ''' Write rows data to a CSV file (with or without header) '''
+        if not quoting:
+            quoting = csv.QUOTE_MINIMAL
+        with open(file_name, 'w') as csvfile:
+            if not header:
+                writer = csv.writer(csvfile, dialect=dialect, quoting=quoting)
+                for row in rows:
+                    writer.writerow(row)
+            else:
+                writer = csv.DictWriter(csvfile, dialect=dialect, fieldnames=header, quoting=quoting, extrasaction=extrasaction)
+                writer.writeheader()
+                for row in rows:
+                    writer.writerow(row)
+
+    @staticmethod
+    def write_tsv(file_name, rows, quoting=csv.QUOTE_ALL):
+        CSV.write(file_name, rows, dialect='excel-tab', quoting=quoting)
+
+
+#-------------------------------------------------------------------------------
+# MAIN
+#-------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    print("This is a library, not an application")
---
+++ test/test_io.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Test script for readers
+Latest version can be found at https://github.com/letuananh/chirptext
+
+References:
+    Python unittest documentation:
+        https://docs.python.org/3/library/unittest.html
+    Python documentation:
+        https://docs.python.org/
+    PEP 0008 - Style Guide for Python Code
+        https://www.python.org/dev/peps/pep-0008/
+    PEP 0257 - Python Docstring Conventions:
+        https://www.python.org/dev/peps/pep-0257/
+
+@author: Le Tuan Anh <tuananh.ke@gmail.com>
+'''
+
+# Copyright (c) 2017, Le Tuan Anh <tuananh.ke@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+__author__ = "Le Tuan Anh"
+__email__ = "<tuananh.ke@gmail.com>"
+__copyright__ = "Copyright 2017, chirptext"
+__license__ = "MIT"
+__maintainer__ = "Le Tuan Anh"
+__version__ = "0.1"
+__status__ = "Prototype"
+__credits__ = []
+
+########################################################################
+
+import os
+import unittest
+from chirptext.anhxa import to_json, to_obj
+from chirptext.io import CSV
+
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA = os.path.join(TEST_DIR, 'data')
+TEST_CSV = os.path.join(TEST_DATA, 'test.csv')
+
+
+#-------------------------------------------------------------------------------
+# DATA STRUCTURES
+#-------------------------------------------------------------------------------
+
+class Person:
+
+    def __init__(self, first, last='', idno=''):
+        self.first = first
+        self.last = last
+        self.idno = idno
+
+    @staticmethod
+    def parse(full_name):
+        parts = full_name.split()
+        if len(parts) == 1:
+            return Person(parts[0])
+        elif len(parts) == 2:
+            return Person(parts[0], parts[1])
+        else:
+            return Person(parts[0], parts[1], parts[2])
+
+    def __eq__(self, other):
+        return type(other) == Person and self.first == other.first and self.last == other.last and self.idno == other.idno
+
+
+#-------------------------------------------------------------------------------
+# TESTS
+#-------------------------------------------------------------------------------
+
+class TestReaders(unittest.TestCase):
+
+    def test_csv(self):
+        print("Test CSV io")
+        # test writing numbers => strings
+        data = ((1, 2, 3), (4, 5, 6))
+        CSV.write(TEST_CSV, data)
+        indata = CSV.read(TEST_CSV)
+        self.assertEqual(indata, [['1', '2', '3'], ['4', '5', '6']])
+
+        # test writing strings with spaces
+        data = [["Doraemon", "Nobita Nobi", "Shizuka Minamoto", "Dorami"],
+                ["Takeshi Goda", "Suneo Honekawa", "Jaiko", "Hidetoshi Dekisugi"]]
+        CSV.write_tsv(TEST_CSV, data)
+        indata = CSV.read(TEST_CSV)
+        self.assertEqual(data, indata)
+
+        # writing only 1 string with space?
+        data = [['Takeshi Goda']]
+        CSV.write_tsv(TEST_CSV, data, quoting=None)
+        indata = CSV.read(TEST_CSV)
+        expected = [['Takeshi', 'Goda']]  # not quoting => CSV reader cannot guess the format
+        self.assertEqual(indata, expected)
+        # read file with a proper dialect
+        indata = CSV.read(TEST_CSV, 'excel-tab')
+        self.assertEqual(data, indata)
+
+        # write file with forced quoting (default option)
+        data = [['Takeshi Goda']]
+        CSV.write_tsv(TEST_CSV, data)
+        indata = CSV.read(TEST_CSV)
+        self.assertEqual(data, indata)
+
+        # with header
+        names = ["Doraemon - D001", "Nobita Nobi N001", "Shizuka Minamoto M001", "Dorami - D002",
+                 "Takeshi Goda G001", "Suneo Honekawa H001", "Jaiko - G002", "Hidetoshi Dekisugi D003"]
+        persons = [Person.parse(name) for name in names]
+        header = ['first', 'last']
+        CSV.write(TEST_CSV, [to_json(p) for p in persons], header=header)
+        inrows = CSV.read(TEST_CSV, header=header)
+        expected = [{'first': 'Doraemon', 'last': '-'}, {'first': 'Nobita', 'last': 'Nobi'}, {'first': 'Shizuka', 'last': 'Minamoto'}, {'first': 'Dorami', 'last': '-'}, {'first': 'Takeshi', 'last': 'Goda'}, {'first': 'Suneo', 'last': 'Honekawa'}, {'first': 'Jaiko', 'last': '-'}, {'first': 'Hidetoshi', 'last': 'Dekisugi'}]
+        self.assertEqual(inrows, expected)
+        # read in as objects
+        inpersons = [to_obj(Person, row) for row in CSV.read(TEST_CSV, header=header)]
+        expected_names = [to_obj(Person, row) for row in expected]  # without idno
+        self.assertEqual(inpersons, expected_names)
+
+        # with idno
+        header = ['first', 'last', 'idno']
+        CSV.write(TEST_CSV, [to_json(p) for p in persons], header=header)
+        inpersons = [to_obj(Person, row) for row in CSV.read(TEST_CSV, header=header)]
+        self.assertEqual(persons, inpersons)
+
+
+#-------------------------------------------------------------------------------
+# MAIN
+#-------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    unittest.main()
