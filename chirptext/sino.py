959f4d2b230367e2ef9f2cc5f6975940e71ca0ef
Le Tuan Anh <tuananh.ke@gmail.com>
2017-08-06 23:49:00 +0800

    Add sino module with Kangxi radicals

---
+++ chirptext/sino.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+'''
+Tools for processing Chinese
+Latest version can be found at https://github.com/letuananh/chirptext
+
+References:
+    Python documentation:
+        https://docs.python.org/
+    PEP 0008 - Style Guide for Python Code
+        https://www.python.org/dev/peps/pep-0008/
+    PEP 257 - Python Docstring Conventions:
+        https://www.python.org/dev/peps/pep-0257/
+
+@author: Le Tuan Anh <tuananh.ke@gmail.com>
+'''
+
+# Copyright (c) 2017, Le Tuan Anh <tuananh.ke@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+__author__ = "Le Tuan Anh"
+__email__ = "<tuananh.ke@gmail.com>"
+__copyright__ = "Copyright 2017, chirptext"
+__license__ = "MIT"
+__maintainer__ = "Le Tuan Anh"
+__version__ = "0.1"
+__status__ = "Prototype"
+__credits__ = []
+
+########################################################################
+
+import os
+import logging
+from collections import defaultdict as dd
+from collections import OrderedDict as odict
+from .io import CSV
+from .anhxa import to_obj, to_json
+
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+MY_DIR = os.path.dirname(os.path.realpath(__file__))
+KANGXI_FILE = os.path.join(MY_DIR, 'sino_data', 'kangxi.csv')
+
+#-------------------------------------------------------------------------------
+# DATA STRUCTURES
+#-------------------------------------------------------------------------------
+
+KANGXI_FIELDS = ["idseq", "radical", "variants", "strokes", "meaning", "pinyin", "hanviet", "hiragana", "romaji", "hangeul", "romaja", "frequency", "simplified", "examples"]
+
+
+#-------------------------------------------------------------------------------
+# FUNCTIONS
+#-------------------------------------------------------------------------------
+
+class Radical(object):
+
+    ''' Chinese Radical
+        Source: https://en.wikipedia.org/wiki/Kangxi_radical#Table_of_radicals
+    '''
+    def __init__(self, idseq, radical, variants, strokes, meaning, pinyin, hanviet, hiragana, romaji, hangeul, romaja, frequency, simplified, examples):
+        self.idseq = idseq
+        self.radical = radical
+        self.variants = variants
+        self.strokes = strokes
+        self.meaning = meaning
+        self.pinyin = pinyin
+        self.hanviet = hanviet
+        self.hiragana = hiragana
+        self.romaji = romaji
+        self.hangeul = hangeul
+        self.romaja = romaja
+        self.frequency = frequency
+        self.simplified = simplified
+        self.examples = examples
+
+    def to_json(self):
+        return to_json(self)
+
+    __KANGXI_MAP = None
+
+    @staticmethod
+    def kangxi():
+        if not Radical.__KANGXI_MAP:
+            kxs = CSV.read(KANGXI_FILE, header=True)
+            Radical.__KANGXI_MAP = KangxiMap(kxs)
+        return Radical.__KANGXI_MAP
+
+
+class KangxiMap:
+
+    def __init__(self, rads=None):
+        self.radicals = []
+        self.rad_map = {}     # kangxi.radical -> kangxi object
+        self.id_rad_map = {}  # idseq ('1', '2', i.e. string) -> rad object
+        self.strokes_map = dd(list)  # map strokes => radicals
+        if rads:
+            for rad in rads:
+                self.add(to_obj(Radical, rad))
+
+    @property
+    def all(self):
+        return [r.radical for r in self.radicals]
+
+    @property
+    def strokes(self):
+        return {sc: [r.radical for r in rads] for sc, rads in self.strokes_map.items()}
+
+    def __getitem__(self, key):
+        if key in self.rad_map:
+            return self.rad_map[key]
+        elif key in self.id_rad_map:
+            return self.id_rad_map[key]
+        else:
+            return self.radicals[key]  # by list index
+
+    def __contains__(self, key):
+        return key in self.rad_map or key in self.id_rad_map
+
+    def add(self, rad):
+        self.radicals.append(rad)
+        self.rad_map[rad.radical] = rad
+        self.id_rad_map[rad.idseq] = rad
+        self.strokes_map[int(rad.strokes)].append(rad)
+        # map variants & simplified
+        if rad.variants:
+            for v in rad.variants.split():
+                self.rad_map[v] = rad
+        if rad.simplified:
+            for s in rad.simplified.split():
+                self.rad_map[s] = rad
---
+++ test/test_sino.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Test script for sino module
+Latest version can be found at https://github.com/letuananh/chirptext
+
+References:
+    Python unittest documentation:
+        https://docs.python.org/3/library/unittest.html
+    Python documentation:
+        https://docs.python.org/
+    PEP 0008 - Style Guide for Python Code
+        https://www.python.org/dev/peps/pep-0008/
+    PEP 0257 - Python Docstring Conventions:
+        https://www.python.org/dev/peps/pep-0257/
+
+@author: Le Tuan Anh <tuananh.ke@gmail.com>
+'''
+
+# Copyright (c) 2017, Le Tuan Anh <tuananh.ke@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+__author__ = "Le Tuan Anh"
+__email__ = "<tuananh.ke@gmail.com>"
+__copyright__ = "Copyright 2017, chirptext"
+__license__ = "MIT"
+__maintainer__ = "Le Tuan Anh"
+__version__ = "0.1"
+__status__ = "Prototype"
+__credits__ = []
+
+########################################################################
+
+import os
+import unittest
+from chirptext.sino import Radical
+
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA = os.path.join(TEST_DIR, 'data')
+
+
+#-------------------------------------------------------------------------------
+# DATA STRUCTURES
+#-------------------------------------------------------------------------------
+
+class TestSino(unittest.TestCase):
+
+    def test_sino(self):
+        rads = Radical.kangxi()
+        kiiro = rads[200]
+        self.assertEqual(rads['黃'], kiiro)
+        self.assertEqual(rads['201'], kiiro)
+        self.assertEqual(rads.all, ['一', '丨', '丶', '丿', '乙', '亅', '二', '亠', '人', '儿', '入', '八', '冂', '冖', '冫', '几', '凵', '刀', '力', '勹', '匕', '匚', '匸', '十', '卜', '卩', '厂', '厶', '又', '口', '囗', '土', '士', '夂', '夊', '夕', '大', '女', '子', '宀', '寸', '小', '尢', '尸', '屮', '山', '巛', '工', '己', '巾', '干', '幺', '广', '廴', '廾', '弋', '弓', '彐', '彡', '彳', '心', '戈', '戶', '手', '支', '攴', '文', '斗', '斤', '方', '无', '日', '曰', '月', '木', '欠', '止', '歹', '殳', '毋', '比', '毛', '氏', '气', '水', '火', '爪', '父', '爻', '爿', '片', '牙', '牛', '犬', '玄', '玉', '瓜', '瓦', '甘', '生', '用', '田', '疋', '疒', '癶', '白', '皮', '皿', '目', '矛', '矢', '石', '示', '禸', '禾', '穴', '立', '竹', '米', '糸', '缶', '网', '羊', '羽', '老', '而', '耒', '耳', '聿', '肉', '臣', '自', '至', '臼', '舌', '舛', '舟', '艮', '色', '艸', '虍', '虫', '血', '行', '衣', '襾', '見', '角', '言', '谷', '豆', '豕', '豸', '貝', '赤', '走', '足', '身', '車', '辛', '辰', '辵', '邑', '酉', '釆', '里', '金', '長', '門', '阜', '隶', '隹', '雨', '青', '非', '面', '革', '韋', '韭', '音', '頁', '風', '飛', '食', '首', '香', '馬', '骨', '高', '髟', '鬥', '鬯', '鬲', '鬼', '魚', '鳥', '鹵', '鹿', '麥', '麻', '黃', '黍', '黑', '黹', '黽', '鼎', '鼓', '鼠', '鼻', '齊', '齒', '龍', '龜', '龠'])
+        self.assertEqual(rads.strokes, {1: ['一', '丨', '丶', '丿', '乙', '亅'], 2: ['二', '亠', '人', '儿', '入', '八', '冂', '冖', '冫', '几', '凵', '刀', '力', '勹', '匕', '匚', '匸', '十', '卜', '卩', '厂', '厶', '又'], 3: ['口', '囗', '土', '士', '夂', '夊', '夕', '大', '女', '子', '宀', '寸', '小', '尢', '尸', '屮', '山', '巛', '工', '己', '巾', '干', '幺', '广', '廴', '廾', '弋', '弓', '彐', '彡', '彳'], 4: ['心', '戈', '戶', '手', '支', '攴', '文', '斗', '斤', '方', '无', '日', '曰', '月', '木', '欠', '止', '歹', '殳', '毋', '比', '毛', '氏', '气', '水', '火', '爪', '父', '爻', '爿', '片', '牙', '牛', '犬'], 5: ['玄', '玉', '瓜', '瓦', '甘', '生', '用', '田', '疋', '疒', '癶', '白', '皮', '皿', '目', '矛', '矢', '石', '示', '禸', '禾', '穴', '立'], 6: ['竹', '米', '糸', '缶', '网', '羊', '羽', '老', '而', '耒', '耳', '聿', '肉', '臣', '自', '至', '臼', '舌', '舛', '舟', '艮', '色', '艸', '虍', '虫', '血', '行', '衣', '襾'], 7: ['見', '角', '言', '谷', '豆', '豕', '豸', '貝', '赤', '走', '足', '身', '車', '辛', '辰', '辵', '邑', '酉', '釆', '里'], 8: ['金', '長', '門', '阜', '隶', '隹', '雨', '青', '非'], 9: ['面', '革', '韋', '韭', '音', '頁', '風', '飛', '食', '首', '香'], 10: ['馬', '骨', '高', '髟', '鬥', '鬯', '鬲', '鬼'], 11: ['魚', '鳥', '鹵', '鹿', '麥', '麻'], 12: ['黃', '黍', '黑', '黹'], 13: ['黽', '鼎', '鼓', '鼠'], 14: ['鼻', '齊'], 15: ['齒'], 16: ['龍', '龜'], 17: ['龠']})
+
+
+#-------------------------------------------------------------------------------
+# MAIN
+#-------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    unittest.main()
