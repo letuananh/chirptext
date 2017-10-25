0bc26a6d5735f294267b2b3b476e95de5fc9173b
Le Tuan Anh <tuananh.ke@gmail.com>
2017-10-25 16:12:15 +0800

    Add detokenize() to StringTool

--- chirptext/leutile.py
+++ chirptext/leutile.py
@@ -282,6 +282,29 @@ class StringTool:
     def to_str(a_str):
         return str(a_str) if a_str else ''
 
+    @staticmethod
+    def detokenize(tokens):
+        sentence_text = ' '.join(tokens)
+        sentence_text = sentence_text.replace(" , , ", ", ")
+        sentence_text = sentence_text.replace(' , ', ', ').replace('“ ', '“').replace(' ”', '”')
+        sentence_text = sentence_text.replace(' ! ', '! ').replace(" 'll ", "'ll ").replace(" 've ", "'ve ").replace(" 're ", "'re ").replace(" 'd ", "'d ")
+        sentence_text = sentence_text.replace(" 's ", "'s ")
+        sentence_text = sentence_text.replace(" 'm ", "'m ")
+        sentence_text = sentence_text.replace(" ' ", "' ")
+        sentence_text = sentence_text.replace(" ; ", "; ")
+        sentence_text = sentence_text.replace(" : ", ": ")
+        sentence_text = sentence_text.replace("( ", "(")
+        sentence_text = sentence_text.replace(" )", ")")
+        sentence_text = sentence_text.replace(" ?", "?")
+        sentence_text = sentence_text.replace(" n't ", "n't ")
+        sentence_text = sentence_text.replace("  ", " ")
+        sentence_text = sentence_text.replace('``', "“").replace("''", "”")
+        sentence_text = sentence_text.replace("“ ", "“").replace(" ”", "”")
+        if sentence_text[-2:] in (' .', ' :', ' ?', ' !', " ;"):
+            sentence_text = sentence_text[:-2] + sentence_text[-1]
+        sentence_text = sentence_text.strip()
+        return sentence_text
+
 
 ###############################################################################
 
