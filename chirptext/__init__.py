a921f2e8f45c345a77ee714595b4fb5260c180e5
Le Tuan Anh <tuananh.ke@gmail.com>
2018-04-04 12:07:39 +0800

    Remove long LICENSE from the headers of code files

--- chirptext/__init__.py
+++ chirptext/__init__.py
@@ -5,52 +5,15 @@ Chirp Text - Minimalist Text Processing Library
 
 Latest version can be found at https://github.com/letuananh/chirptext
 
-References:
-    Python documentation:
-        https://docs.python.org/
-    PEP 0008 - Style Guide for Python Code
-        https://www.python.org/dev/peps/pep-0008/
-    PEP 0257 - Python Docstring Conventions:
-        https://www.python.org/dev/peps/pep-0257/
-
-@author: Le Tuan Anh <tuananh.ke@gmail.com>
-@license: MIT
+:copyright: (c) 2012 Le Tuan Anh <tuananh.ke@gmail.com>
+:license: MIT, see LICENSE for more details.
 '''
 
-# Copyright (c) 2012-2017, Le Tuan Anh <tuananh.ke@gmail.com>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-__author__ = "Le Tuan Anh"
-__email__ = "tuananh.ke@gmail.com"
-__copyright__ = "Copyright 2017, chirptext"
-__credits__ = []
-__license__ = "MIT License"
-__description__ = "ChirpText is a collection of text processing tools for Python."
-__url__ = "https://github.com/letuananh/chirptext"
-__maintainer__ = "Le Tuan Anh"
-__version_major__ = "0.1"
-__version__ = "{}a12".format(__version_major__)
-__version_long__ = "{} - Alpha".format(__version_major__)
-__status__ = "Prototype"
-
 ########################################################################
+
+from .__version__ import __author__, __email__, __copyright__, __maintainer__
+from .__version__ import __credits__, __license__, __description__, __url__
+from .__version__ import __version_major__, __version_long__, __version__, __status__
 
 from .leutile import uniquify, header, confirm
 from .leutile import Timer, Counter, FileHub, TextReport, FileHelper, AppConfig
@@ -63,4 +26,5 @@ __all__ = ["uniquify", "header", "confirm",
            "Timer", "Counter",
            "TextReport", "FileHub", "FileHelper", "AppConfig",
            "JiCache", "SmartURL", "WebHelper",
-           "DataObject", "ttl"]
+           "DataObject", "ttl",
+           "__version__", "__author__", "__description__", "__copyright__"]
---
+++ chirptext/__version__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# chirptext's package version information
+__author__ = "Le Tuan Anh"
+__email__ = "tuananh.ke@gmail.com"
+__copyright__ = "Copyright (c) 2012, Le Tuan Anh"
+__credits__ = []
+__license__ = "MIT License"
+__description__ = "ChirpText is a collection of text processing tools for Python."
+__url__ = "https://github.com/letuananh/chirptext"
+__maintainer__ = "Le Tuan Anh"
+__version_major__ = "0.1"
+__version__ = "{}a12".format(__version_major__)
+__version_long__ = "{} - Alpha".format(__version_major__)
+__status__ = "Prototype"
