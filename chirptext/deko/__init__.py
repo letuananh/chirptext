526de1752caef9081cc5cc152da3ed19e3cb8bc8
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-17 22:31:23 +0800

    deko.dekoigo is fully working now

--- chirptext/deko/__init__.py
+++ chirptext/deko/__init__.py
@@ -15,15 +15,49 @@ Convenient Japanese text parser that produces results in TTL format
 # :license: MIT, see LICENSE for more details.
 
 from . import mecab
+from . import dekoigo
 from . import util
 
 # allow mecab config
 set_mecab_bin = mecab._register_mecab_loc
 get_mecab_bin = mecab._get_mecab_loc
-parse = mecab.parse
-parse_doc = mecab.parse_doc
-tokenize = mecab.tokenize
-tokenize_sent = mecab.tokenize_sent
+
+
+def engines():
+    _engines = []
+    try:
+        mv = mecab.version()
+        if mv:
+            _engines.append(("mecab", mecab))
+    except Exception:
+        pass
+    if dekoigo.igo_available():
+        _engines.append(("igo", dekoigo))
+    return _engines
+
+
+def _locate_engine(*args, **kwargs):
+    _engines = engines()
+    if not _engines:
+        raise RuntimeError("There is no Japanese parser available")
+    else:
+        return _engines[0][1]
+
+
+def parse(*args, **kwargs):
+    return _locate_engine().parse(*args, **kwargs)
+
+
+def parse_doc(*args, **kwargs):
+    return _locate_engine().parse_doc(*args, **kwargs)
+
+
+def tokenize(*args, **kwargs):
+    return _locate_engine().tokenize(*args, **kwargs)
+
+
+def tokenize_sent(*args, **kwargs):
+    return _locate_engine().tokenize_sent(*args, **kwargs)
 
 
 def analyse(content, splitlines=True, format=None, **kwargs):
@@ -46,7 +80,7 @@ def analyse(content, splitlines=True, format=None, **kwargs):
     return final
 
 
-__all__ = ['set_mecab_bin', 'get_mecab_bin',
+__all__ = ['set_mecab_bin', 'get_mecab_bin', 'engines',
            'parse', 'parse_doc', 'tokenize', 'tokenize_sent', 'analyse']
 
 
